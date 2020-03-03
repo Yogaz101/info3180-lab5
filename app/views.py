ede36c2991b5071d4f9872204125650d05c4686c
Yogaz101 <43712799+Yogaz101@users.noreply.github.com>
2020-03-03 02:12:52 -0500

    last commit

--- app/views.py
+++ app/views.py
@@ -53,12 +53,6 @@ def login():
             else:
                 flash("Unsuccessful Login", "Unsuccesful")
     return render_template("login.html", form=form)
-            # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
-    return render_template("login.html", form=form)
 
 
 # user_loader callback. This callback is used to reload the user object from
